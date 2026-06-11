d3f7c0655afb6accc11890e7cda2cd8709f21773
gosu <godblessosu@yahoo.com>
2026-06-11 17:14:29 +0100

    test client

--- main.go
+++ main.go
@@ -2,27 +2,40 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 )
 
 func main() {
-	transport := &http.Transport{
-		ResponseHeaderTimeout: 5 * time.Second, // Time to wait for server&#39;s first response header
-		ExpectContinueTimeout: 1 * time.Second, // Time to wait for a response after sending an `Expect: 100-continue` header
+	// transport := &http.Transport{
+	// 	ResponseHeaderTimeout: 5 * time.Second, // Time to wait for server&#39;s first response header
+	// 	ExpectContinueTimeout: 1 * time.Second, // Time to wait for a response after sending an `Expect: 100-continue` header
+	// }
+
+	// client := &http.Client{
+	// 	Transport: transport,
+	// 	Timeout:   15 * time.Second, // Still advisable to set an overall timeout
+	// }
+
+	// resp, err := client.Get("https://api.example.com/data")
+	// if err != nil {
+	// 	fmt.Println("Request failed:", err)
+	// 	return
+	// }
+	// defer resp.Body.Close()
+
+	// fmt.Println("Response status:", resp.Status)
+
+	client := http.Client{
+		Timeout: 10 * time.Second,
 	}
 
-	client := &http.Client{
-		Transport: transport,
-		Timeout:   15 * time.Second, // Still advisable to set an overall timeout
-	}
-
-	resp, err := client.Get("https://api.example.com/data")
+	resp, err := client.Get("https://api.github.com/")
 	if err != nil {
-		fmt.Println("Request failed:", err)
-		return
+		log.Fatalf("Request failed\n%v", err)
 	}
 	defer resp.Body.Close()
 
-	fmt.Println("Response status:", resp.Status)
+	fmt.Println(resp.Status)
 }
